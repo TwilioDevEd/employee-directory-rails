a2f6be4fe8c7f230f489e6e1eb7af7f0ab2451a5
Agustin Camino <agustin.camino@gmail.com>
2016-03-11 17:06:49 -0500

    Add migration to create users

---
+++ db/migrate/20160311212836_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string    :first_name,          null: false
+      t.string    :last_name,           null: false
+      t.string    :job_title,           null: false
+      t.string    :city,                null: false
+      t.string    :phone_number,        null: false
+      t.string    :email,               null: false
+      t.timestamps                      null: false
+    end
+  end
+end
