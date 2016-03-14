9c7ec3559f57ae11e86ff2cdeb64fd4d04bb41bc
Agustin Camino <agustin.camino@gmail.com>
2016-03-14 08:53:04 -0500

    Enable pg_trgm extension

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160311212836) do
+ActiveRecord::Schema.define(version: 20160314133534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_trgm"
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name",   null: false
