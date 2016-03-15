205a8c37e0ce40768f017769ad999d5354554d8f
Agustin Camino <agustin.camino@gmail.com>
2016-03-15 14:45:49 -0500

    Use scope syntactic sugar

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,7 +1,7 @@
 class Employee < ActiveRecord::Base
   fuzzily_searchable :name
 
-  def self.perfect_match(query)
+  scope :perfect_match, -> query do
     first_name, last_name = query.downcase.split(' ')
     where("lower(first_name) = ? AND lower(last_name) = ?",
           first_name, last_name)
