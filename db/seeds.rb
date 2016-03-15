c82d7504d4215295cbdde7dae3bd498989a55cca
Agustin Camino <agustin.camino@gmail.com>
2016-03-15 10:57:23 -0500

    Add a new employee to the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,23 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #
-Employee.create(
-  first_name: 'Robert',
-  last_name: 'Williams',
-  job_title: 'Facility and Employee Experiences Coordinator',
-  city: 'San Francisco',
-  phone_number: '+1-202-555-0143',
-  email: 'rwilliams@twilio.com',
-  image_url: 'http://bit.ly/twilio-rob-williams')
+Employee.create([
+  {
+    first_name: 'Robin',
+    last_name: 'Williams',
+    job_title: 'Facility and Employee Experiences Coordinator',
+    city: 'San Francisco',
+    phone_number: '+1-202-555-0143',
+    email: 'rwilliams@twilio.com',
+    image_url: 'http://bit.ly/twilio-rob-williams'
+  },
+  {
+    first_name: 'Frankie',
+    last_name: 'Muniz',
+    job_title: 'QA Manager',
+    city: 'New York',
+    phone_number: '+1-202-928-0143',
+    email: 'fmuniz@twilio.com',
+    image_url: 'http://bit.ly/twilio-frank-muniz'
+  }
+])
